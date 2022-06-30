4aea8d122ca46de746fbf82a8187cffcb0f7374b
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 12:47:13 +0300

    Pawns can't move in occupied squares

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -12,22 +12,30 @@ namespace Chessington.GameEngine.Pieces
         {
             List<Square> availableMoves = new List<Square>();
             
-            if (this.Player == Player.White)
+            if (this.Player == Player.White && 
+                board.GetPiece(new Square(board.FindPiece(this).Row - 1, board.FindPiece(this).Col)) == null)
             {
-                if (board.FindPiece(this).Row == 7)
+                if (board.FindPiece(this).Row == 7 && 
+                    board.GetPiece(new Square(board.FindPiece(this).Row - 2, board.FindPiece(this).Col)) == null)
                 { 
                     availableMoves.Add(new Square(board.FindPiece(this).Row - 2, board.FindPiece(this).Col));
                 }
+               
+                
                 availableMoves.Add(new Square(board.FindPiece(this).Row - 1, board.FindPiece(this).Col));
+                
             }
-            else if (this.Player == Player.Black)
+            else if (this.Player == Player.Black 
+                     && board.GetPiece(new Square(board.FindPiece(this).Row + 1, board.FindPiece(this).Col)) == null)
             {
-                if (board.FindPiece(this).Row == 1)
+                if (board.FindPiece(this).Row == 1 &&
+                    board.GetPiece(new Square(board.FindPiece(this).Row + 2, board.FindPiece(this).Col)) == null)
                 {
-                    
                     availableMoves.Add(new Square(board.FindPiece(this).Row + 2, board.FindPiece(this).Col));
                 }
-                availableMoves.Add(new Square(board.FindPiece(this).Row + 1, board.FindPiece(this).Col));
+
+                availableMoves.Add( new Square(board.FindPiece(this).Row + 1, board.FindPiece(this).Col));
+                
             }
             return availableMoves;
         }
