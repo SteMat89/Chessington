6168c12e82e6a219fdb170ecc3dc85188312a254
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 11:04:13 +0300

    Pawns can move one space in front of them.

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,7 +10,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            
+            if (this.Player == Player.White)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row - 1, board.FindPiece(this).Col));
+            }
+            else if (this.Player == Player.Black)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row + 1, board.FindPiece(this).Col));
+            }
+            return availableMoves;
         }
     }
 }
