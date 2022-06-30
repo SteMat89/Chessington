901b271ff3fb49242d385b070c741b6de12fbfd8
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 12:01:31 +0300

    Added rules for knight

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -10,7 +10,17 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+            
+            int[] row = new int[] { 2, 1, -1, -2, -2, -1, 1, 2};
+            int[] col = new int[] { 1, 2, 2, 1, -1, -2, -2, -1};
+
+            for (int i = 0; i < 8; i++)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row + row[i], 
+                    board.FindPiece(this).Col + col[i]));
+            }
+            return availableMoves;
         }
     }
 }
