046e0f59c45c592cc33e0025361783ad7085ae67
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 11:50:44 +0300

    Added rules for Queen

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -9,8 +9,49 @@ namespace Chessington.GameEngine.Pieces
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
-        {
-            return Enumerable.Empty<Square>();
+        { 
+            List<Square> availableMoves = new List<Square>();
+            
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1; i < 8 && j < 8; i++, j++)
+            {
+                availableMoves.Add(new Square(i, j));
+            }
+            
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1; i < 8 && j >= 0; i++, j--)
+            {
+                availableMoves.Add(new Square(i, j));
+            }
+            
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1; i >= 0 && j < 8; i--, j++)
+            {
+                availableMoves.Add(new Square(i, j));
+            }
+            
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                availableMoves.Add(new Square(i, j));
+            }
+            
+            for (int i = board.FindPiece(this).Col + 1; i < 8; i++)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+            }
+            
+            for (int i = board.FindPiece(this).Col - 1; i >= 0; i--)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+            }
+            
+            for (int i = board.FindPiece(this).Row + 1; i < 8; i++)
+            {
+                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+            }
+            
+            for (int i = board.FindPiece(this).Row - 1; i >= 0; i--)
+            {
+                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+            }
+            return availableMoves;
         }
     }
 }
