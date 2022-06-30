d7b404c52b8a96d7030c8ed0124398ef735ae1ef
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 11:24:23 +0300

    Added rules for rook

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,29 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            List<Square> availableMoves = new List<Square>();
+
+            for (int i = board.FindPiece(this).Col + 1; i < 8; i++)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+            }
+            
+            for (int i = board.FindPiece(this).Col - 1; i >= 0; i--)
+            {
+                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+            }
+            
+            for (int i = board.FindPiece(this).Row + 1; i < 8; i++)
+            {
+                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+            }
+            
+            for (int i = board.FindPiece(this).Row - 1; i >= 0; i--)
+            {
+                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+            }
+            
+            return availableMoves;
         }
     }
 }
