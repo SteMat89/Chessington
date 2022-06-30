bc231f968e05de93ad1cc176514bffce04a82d6f
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 13:25:42 +0300

    Added rules for queen

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -12,42 +12,53 @@ namespace Chessington.GameEngine.Pieces
         { 
             List<Square> availableMoves = new List<Square>();
             
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1; i < 8 && j < 8; i++, j++)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1;
+                 i < 8 && j < 8 && board.GetPiece(new Square(i, j)) == null; i++, j++)
             {
                 availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1; i < 8 && j >= 0; i++, j--)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1;
+                 i < 8 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i++, j--)
             {
                 availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1; i >= 0 && j < 8; i--, j++)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1;
+                 i >= 0 && j < 8 && board.GetPiece(new Square(i, j)) == null; i--, j++)
             {
                 availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1; i >= 0 && j >= 0; i--, j--)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1;
+                 i >= 0 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i--, j--)
             {
-                availableMoves.Add(new Square(i, j));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));
+                }
             }
             
-            for (int i = board.FindPiece(this).Col + 1; i < 8; i++)
+            for (int i = board.FindPiece(this).Col + 1; 
+                 i < 8 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i++)
             {
                 availableMoves.Add(new Square(board.FindPiece(this).Row, i));
             }
             
-            for (int i = board.FindPiece(this).Col - 1; i >= 0; i--)
+            for (int i = board.FindPiece(this).Col - 1; 
+                 i >= 0 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i--)
             {
                 availableMoves.Add(new Square(board.FindPiece(this).Row, i));
             }
             
-            for (int i = board.FindPiece(this).Row + 1; i < 8; i++)
+            for (int i = board.FindPiece(this).Row + 1;
+                 i < 8 && board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null; i++)
             {
                 availableMoves.Add(new Square(i, board.FindPiece(this).Col));
             }
             
-            for (int i = board.FindPiece(this).Row - 1; i >= 0; i--)
+            for (int i = board.FindPiece(this).Row - 1;
+                 i >= 0 && board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null; i--)
             {
                 availableMoves.Add(new Square(i, board.FindPiece(this).Col));
             }
