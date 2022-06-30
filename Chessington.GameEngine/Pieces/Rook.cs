3f3ba84c4fe66206d51a91c1c56f9b5471a45ac4
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 15:18:12 +0300

    Rooks can capture enemy pieces

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -12,28 +12,72 @@ namespace Chessington.GameEngine.Pieces
         {
             List<Square> availableMoves = new List<Square>();
 
-            for (int i = board.FindPiece(this).Col + 1; 
-                 i < 8 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i++)
+            for (int i = board.FindPiece(this).Col + 1; i < 8; i++)
             {
-                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                if (board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));    
+                }
+                else if (board.GetPiece(new Square(board.FindPiece(this).Row, i)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Col - 1;
-                 i >= 0 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i--)
+            for (int i = board.FindPiece(this).Col - 1; i >= 0; i--)
             {
-                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                if (board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));    
+                }
+                else if (board.GetPiece(new Square(board.FindPiece(this).Row, i)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row + 1;
-                 i < 8 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i++)
+            for (int i = board.FindPiece(this).Row + 1; i < 8; i++)
             {
-                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                if (board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));    
+                }
+                else if (board.GetPiece(new Square(i, board.FindPiece(this).Col)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row - 1;
-                 i >= 0 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i--)
+            for (int i = board.FindPiece(this).Row - 1; i >= 0; i--)
             {
-                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                if (board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));    
+                }
+                else if (board.GetPiece(new Square(i, board.FindPiece(this).Col)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
             return availableMoves;
