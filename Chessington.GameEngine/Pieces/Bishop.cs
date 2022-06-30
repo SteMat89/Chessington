b792c991f93cb8d9bc078eb5655520a72fd33160
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 15:31:12 +0300

    Added rules for bishop and queen to capture enemy pieces

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -12,28 +12,72 @@ namespace Chessington.GameEngine.Pieces
         {
             List<Square> availableMoves = new List<Square>();
 
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1;
-                 i < 8 && j < 8 && board.GetPiece(new Square(i, j)) == null; i++, j++)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1; i < 8 && j < 8; i++, j++)
             {
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
                     availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1;
-                 i < 8 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i++, j--)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1; i < 8 && j >= 0; i++, j--)
             {
-                availableMoves.Add(new Square(i, j));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1;
-                 i >= 0 && j < 8 && board.GetPiece(new Square(i, j)) == null; i--, j++)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1; i >= 0 && j < 8; i--, j++)
             {
-                availableMoves.Add(new Square(i, j));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1;
-                 i >= 0 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i--, j--)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1; i >= 0 && j >= 0; i--, j--)
             {
-                availableMoves.Add(new Square(i, j));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             return availableMoves;
         }
--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -12,55 +12,140 @@ namespace Chessington.GameEngine.Pieces
         { 
             List<Square> availableMoves = new List<Square>();
             
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1;
-                 i < 8 && j < 8 && board.GetPiece(new Square(i, j)) == null; i++, j++)
-            {
-                availableMoves.Add(new Square(i, j));
-            }
-            
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1;
-                 i < 8 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i++, j--)
-            {
-                availableMoves.Add(new Square(i, j));
-            }
-            
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1;
-                 i >= 0 && j < 8 && board.GetPiece(new Square(i, j)) == null; i--, j++)
-            {
-                availableMoves.Add(new Square(i, j));
-            }
-            
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1;
-                 i >= 0 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i--, j--)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1; i < 8 && j < 8; i++, j++)
             {
                 if (board.GetPiece(new Square(i, j)) == null)
                 {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
                     availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
                 }
             }
             
-            for (int i = board.FindPiece(this).Col + 1; 
-                 i < 8 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i++)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1; i < 8 && j >= 0; i++, j--)
             {
-                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Col - 1; 
-                 i >= 0 && board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null; i--)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1; i >= 0 && j < 8; i--, j++)
             {
-                availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row + 1;
-                 i < 8 && board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null; i++)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1; i >= 0 && j >= 0; i--, j--)
             {
-                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                if (board.GetPiece(new Square(i, j)) == null)
+                {
+                    availableMoves.Add(new Square(i, j));   
+                }
+                else if (board.GetPiece(new Square(i, j)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, j));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             
-            for (int i = board.FindPiece(this).Row - 1;
-                 i >= 0 && board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null; i--)
+            for (int i = board.FindPiece(this).Col + 1; i < 8; i++)
             {
-                availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                if (board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));    
+                }
+                else if (board.GetPiece(new Square(board.FindPiece(this).Row, i)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            for (int i = board.FindPiece(this).Col - 1; i >= 0; i--)
+            {
+                if (board.GetPiece(new Square(board.FindPiece(this).Row, i)) == null)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));    
+                }
+                else if (board.GetPiece(new Square(board.FindPiece(this).Row, i)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row, i));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            for (int i = board.FindPiece(this).Row + 1; i < 8; i++)
+            {
+                if (board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));    
+                }
+                else if (board.GetPiece(new Square(i, board.FindPiece(this).Col)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            for (int i = board.FindPiece(this).Row - 1; i >= 0; i--)
+            {
+                if (board.GetPiece(new Square(i, board.FindPiece(this).Col)) == null)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));    
+                }
+                else if (board.GetPiece(new Square(i, board.FindPiece(this).Col)).Player != this.Player)
+                {
+                    availableMoves.Add(new Square(i, board.FindPiece(this).Col));
+                    break;
+                }
+                else
+                {
+                    break;
+                }
             }
             return availableMoves;
         }
