c2fa4009484c609e91c3fc599fbd0122800ed82c
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 12:59:43 +0300

    Added rules for bishop

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -12,22 +12,26 @@ namespace Chessington.GameEngine.Pieces
         {
             List<Square> availableMoves = new List<Square>();
 
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1; i < 8 && j < 8; i++, j++)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col + 1;
+                 i < 8 && j < 8 && board.GetPiece(new Square(i, j)) == null; i++, j++)
             {
                     availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1; i < 8 && j >= 0; i++, j--)
+            for (int i = board.FindPiece(this).Row + 1, j = board.FindPiece(this).Col - 1;
+                 i < 8 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i++, j--)
             {
                 availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1; i >= 0 && j < 8; i--, j++)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col + 1;
+                 i >= 0 && j < 8 && board.GetPiece(new Square(i, j)) == null; i--, j++)
             {
                 availableMoves.Add(new Square(i, j));
             }
             
-            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1; i >= 0 && j >= 0; i--, j--)
+            for (int i = board.FindPiece(this).Row - 1, j = board.FindPiece(this).Col - 1;
+                 i >= 0 && j >= 0 && board.GetPiece(new Square(i, j)) == null; i--, j--)
             {
                 availableMoves.Add(new Square(i, j));
             }
