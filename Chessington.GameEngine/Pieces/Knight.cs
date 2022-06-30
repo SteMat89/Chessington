d67c08cb4bf6990a15fd14b3888890009cc0b4fe
Stefan Matei <Stefan.Matei@softwire.com>
2022-06-30 15:52:04 +0300

    Rules for knight to capture enemy

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -17,11 +17,18 @@ namespace Chessington.GameEngine.Pieces
 
             for (int i = 0; i < 8; i++)
             {
-                if(board.FindPiece(this).Row + row[i] >= 0 && board.FindPiece(this).Row + row[i] < 8 
-                                                           && board.FindPiece(this).Col + col[i] >=0 
-                                                           && board.FindPiece(this).Col + col[i] < 8)
-                availableMoves.Add(new Square(board.FindPiece(this).Row + row[i], 
-                    board.FindPiece(this).Col + col[i]));
+                if (board.FindPiece(this).Row + row[i] >= 0
+                    && board.FindPiece(this).Row + row[i] < 8
+                    && board.FindPiece(this).Col + col[i] >= 0
+                    && board.FindPiece(this).Col + col[i] < 8 
+                    && (board.GetPiece(new Square(board.FindPiece(this).Row + row[i],
+                        board.FindPiece(this).Col + col[i])) == null 
+                        || board.GetPiece(new Square(board.FindPiece(this).Row + row[i], 
+                            board.FindPiece(this).Col + col[i])).Player != this.Player))
+                {
+                    availableMoves.Add(new Square(board.FindPiece(this).Row + row[i],
+                        board.FindPiece(this).Col + col[i]));
+                }
             }
             return availableMoves;
         }
